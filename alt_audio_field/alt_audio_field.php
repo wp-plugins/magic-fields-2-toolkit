259e3e9c0ecda43950f292f442d8425826fa9cff
Magenta Cuda <Magenta Cuda@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-06-26 21:46:14 +0000

    tagging version 0.4.5.3
    git-svn-id: https://plugins.svn.wordpress.org/magic-fields-2-toolkit/tags/0.4.5.3@939335 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- alt_audio_field/alt_audio_field.php
+++ alt_audio_field/alt_audio_field.php
@@ -104,10 +104,10 @@ EOD
                     'name'        =>  'mf_field[option][align]',
                     'default'     => '',
                     'options'     => array(
-                                        'alignnone'   => 'None',
                                         'aligncenter' => 'Center',
                                         'alignright'  => 'Right',
                                         'alignleft'   => 'Left',
+                                        'alignnone'   => 'None',
                                     ),
                     'add_empty'   => false,
                     'description' => '',
